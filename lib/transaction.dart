21968baac7e6e1664d838957f4ebe750952c22d4
Carlito <carlmanantan122@gmail.com>
2026-07-20 13:33:55 +0800

    Implement JSON serialization for Transaction class
    Added JSON serialization methods to Transaction class.

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -16,4 +16,24 @@ class Transaction {
     this.vault = 'Personal',
     this.tag = '',
   });
+
+  // 1. Converts your data into a JSON map to save to the phone
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'amount': amount,
+    'date': date.toIso8601String(),
+    'vault': vault,
+    'tag': tag,
+  };
+
+  // 2. Converts the saved JSON text back into a Dart object when the app opens
+  factory Transaction.fromJson(Map<String, dynamic> json) => Transaction(
+    id: json['id'],
+    title: json['title'],
+    amount: json['amount'],
+    date: DateTime.parse(json['date']),
+    vault: json['vault'] ?? 'Personal',
+    tag: json['tag'] ?? '',
+  );
 }
